064b831681a7a8e840a7ef61b9e5d54c3d798aaa
skyaki23 <bwctypmjhksk@gmail.com>
2021-07-05 23:07:16 +0800

    修改list-items內容 .ts增加Item物件陣列，測試增加兩個物件。 .html主要增加table，thead為表頭說明，tbody透過*ngFor將.ts的Item物件陣列資料繫結至此。

--- FEDev/src/app/components/list-items/list-items.component.ts
+++ FEDev/src/app/components/list-items/list-items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/app/interfaces/Item';
 
 @Component({
   selector: 'app-list-items',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-items.component.css']
 })
 export class ListItemsComponent implements OnInit {
+
+  listItems: Item[] = [
+    { brand: '微星(MSI)', processor: 'Intel Core i7-11800H', mainMemory: '16GB(8G*2) DDR4-3200', hardDrive: '1 TB M.2 SSD (NVMe PCIe)', graphicsCard: 'GeForce RTX 3070', screenSize: '15.6吋', price: 61900 },
+    { brand: '宏碁(ACER)', processor: 'Intel® Core™ i7-10750H', mainMemory: '1x 16GB DDR4', hardDrive: '512GB PCIe SSD', graphicsCard: 'NVIDIA® GeForce® RTX 2060', screenSize: '15.6吋', price: 38800 }
+  ];
 
   constructor() { }
 
