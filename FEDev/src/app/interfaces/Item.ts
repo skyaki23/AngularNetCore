5f1bb21c90ee2537b1236a9822123ecd379c3ba4
skyaki23 <bwctypmjhksk@gmail.com>
2021-07-05 20:51:56 +0800

    Item.ts增加筆電屬性。 增加services資料夾，新增item.service，作為與API連接的通道。 修改navbar.html，加入筆電廠牌LOGO圖片做navbar排版

--- FEDev/src/app/interfaces/Item.ts
+++ FEDev/src/app/interfaces/Item.ts
@@ -1,3 +1,10 @@
 export interface Item {
-    
+    id?: number;
+    brand?: string;
+    processor?: string;
+    mainMemory?: string;
+    hardDrive?: string;
+    graphicsCard?: string;
+    screenSize?: string;
+    price?: number;
 }
