c1c1f12b0f709b93a3b5feb4d5af0d4472a7c1e6
skyaki23 <bwctypmjhksk@gmail.com>
2021-07-06 21:57:14 +0800

    新增router，並於其它頁面增加routerLink用來對應其router

--- FEDev/src/app/app-routing.module.ts
+++ FEDev/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateUpdateItemComponent } from './components/create-update-item/create-update-item.component';
 import { ListItemsComponent } from './components/list-items/list-items.component';
+import { MenuItemComponent } from './components/menu-item/menu-item.component';
 
 const routes: Routes = [
   { path: '', component: ListItemsComponent },
-  { path: 'CreateUpdate', component: CreateUpdateItemComponent},
+  { path: 'create', component: CreateUpdateItemComponent},
+  { path: 'update/:id', component: CreateUpdateItemComponent },
+  { path: 'menu/:id', component: MenuItemComponent },
   { path: '**', redirectTo: '/', pathMatch: 'full' }
 ];
 
