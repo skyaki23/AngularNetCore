3da407f35eb10d9afe63df9f96bd0d065db84d6c
skyaki23 <bwctypmjhksk@gmail.com>
2021-07-06 21:44:32 +0800

    加入routing路由。 網址亂輸入若無對應頁面可做跳轉，即跳回list-items.component.html。

--- FEDev/src/app/app-routing.module.ts
+++ FEDev/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateUpdateItemComponent } from './components/create-update-item/create-update-item.component';
+import { ListItemsComponent } from './components/list-items/list-items.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListItemsComponent },
+  { path: 'CreateUpdate', component: CreateUpdateItemComponent},
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
