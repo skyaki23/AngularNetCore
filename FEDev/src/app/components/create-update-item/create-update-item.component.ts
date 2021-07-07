82cbfd3832d0a199a4c5eed9df88ce3eb132bb2a
skyaki23 <bwctypmjhksk@gmail.com>
2021-07-07 21:40:30 +0800

    create-update-item.html增加input屬性填寫驗證機制

--- FEDev/src/app/components/create-update-item/create-update-item.component.ts
+++ FEDev/src/app/components/create-update-item/create-update-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Item } from 'src/app/interfaces/Item';
 
 @Component({
   selector: 'app-create-update-item',
@@ -27,5 +28,17 @@ export class CreateUpdateItemComponent implements OnInit {
 
   create() {
     console.log(this.createItem);
+
+    const item: Item = {
+      brand: this.createItem.get('brand')?.value,
+      processor: this.createItem.get('processor')?.value,
+      mainMemory: this.createItem.get('mainMemory')?.value,
+      hardDrive: this.createItem.get('hardDrive')?.value,
+      graphicsCard: this.createItem.get('graphicsCard')?.value,
+      screenSize: this.createItem.get('screenSize')?.value,
+      price: this.createItem.get('price')?.value
+    };
+
+    console.log(item);
   }
 }
