a0678ff8b47d04da8eb508759f421d033d9e5d83
skyaki23 <bwctypmjhksk@gmail.com>
2021-07-09 06:46:10 +0800

    Code First Mirgation，設定ConnectionString、AddDbContext， 建立名為ItemDB的資料庫

---
+++ BEDev/BEDev/Migrations/20210708160428_v1.0.0.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BEDev.Migrations
+{
+    public partial class v100 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Item",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BrandName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Processor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MainMemory = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HardDrive = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GraphicsCard = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ScreenSize = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Item", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Item");
+        }
+    }
+}
--- BEDev/BEDev/Startup.cs
+++ BEDev/BEDev/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +33,9 @@ namespace BEDev
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BEDev", Version = "v1" });
             });
+
+            services.AddDbContext<ItemDbContext>(options =>
+                        options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
