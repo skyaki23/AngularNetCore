5062cd11a94ae191401887fda11cefaa60142b55
skyaki23 <bwctypmjhksk@gmail.com>
2021-07-10 18:02:33 +0800

    list-items.component.ts將原先listItems設還成初始值，於getItems()中將換成從API取得的值。

--- FEDev/src/app/components/list-items/list-items.component.ts
+++ FEDev/src/app/components/list-items/list-items.component.ts
@@ -9,10 +9,7 @@ import { ItemService } from 'src/app/services/item.service';
 })
 export class ListItemsComponent implements OnInit {
 
-  listItems: Item[] = [
-    { brandName: '微星(MSI)', processor: 'Intel Core i7-11800H', mainMemory: '16GB(8G*2) DDR4-3200', hardDrive: '1 TB M.2 SSD (NVMe PCIe)', graphicsCard: 'GeForce RTX 3070', screenSize: '15.6吋', price: 61900 },
-    { brandName: '宏碁(ACER)', processor: 'Intel® Core™ i7-10750H', mainMemory: '1x 16GB DDR4', hardDrive: '512GB PCIe SSD', graphicsCard: 'NVIDIA® GeForce® RTX 2060', screenSize: '15.6吋', price: 38800 }
-  ];
+  listItems: Item[] = [];
 
   constructor(private _itemService: ItemService) { }
 
@@ -23,7 +20,7 @@ export class ListItemsComponent implements OnInit {
   getItems() {
     this._itemService.getListItems().subscribe(
       data => {
-        console.log(data);
+        this.listItems = data;
       },
       error => {
         console.log(error);
